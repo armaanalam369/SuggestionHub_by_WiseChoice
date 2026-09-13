6d4b79f0fdeec4d02afce76e2860e33013b56f91
Armaaaaaan <armaanalam369@gmail.com>
2026-09-13 12:27:35 +0530

    Update compulsory papers with new resources

--- content/compulsory/compulsory.js
+++ content/compulsory/compulsory.js
@@ -2,10 +2,10 @@ const COMPULSORY_PAPERS = {
   name: "Compulsory Papers",
   examDate: "",
   suggestions: [
-    {title: "Compulsory Paper 1 - Practice Set", type: "pdf", url: "PASTE_YOUR_PDF_LINK_HERE"},
-    {title: "Compulsory Paper 1 - Video Explanation", type: "youtube", url: "PASTE_YOUR_YOUTUBE_LINK_HERE"}
+    {title: "Compulsory Paper Notes", type: "pdf", url: "https://drive.google.com/drive/folders/1_tjKNUrVNY-S7EO81Zs_WXMUdhif63RS"},
+    //{title: "Compulsory Paper 1 - Video Explanation", type: "youtube", url: "PASTE_YOUR_YOUTUBE_LINK_HERE"}//
   ],
   materials: [
-    {title: "Compulsory Paper - Notes", type: "pdf", url: "PASTE_YOUR_PDF_LINK_HERE"}
+    {title: "ENVS - Practice Sets", type: "pdf", url: "https://youtube.com/playlist?list=PLbqt9hzBrf2oFdjPNAquv3nMezQv935cY&si=sRn2ncRO44Y_34bT"}
   ]
 };
